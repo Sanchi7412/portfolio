2a61316a7d058f677627c3e13bfbf5b6a97e84ba
yuusuke ishii <111514756+Sanchi7412@users.noreply.github.com>
2024-11-16 18:17:06 +0900

    first commit
    main.dartを簡略化
    
    これから頑張ろうね

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "yamanekosanchi!",
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      body: Center(
+        child:
+          Text("Hello PortFolio"),
+      ),
+    );
+  }
+}
